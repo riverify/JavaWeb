58aa90f655cadd37eed86adce38bc94c9795a7ed
Riverify <CryingRiver@foxmail.com>
2022-09-07 20:25:40 +0800

    多表查询的第一种方式：业务代码方式

--- Mybatis4/src/com/river/mapper/ClazzMapper.java
+++ Mybatis4/src/com/river/mapper/ClazzMapper.java
@@ -4,5 +4,7 @@ import com.river.entity.Clazz;
 
 public interface ClazzMapper {
     // 查询指定学生所在班级号的班级信息　
-    Clazz selectAll(Integer clazzno);
+    Clazz selectOne(Integer clazzno);
+
+
 }
---
+++ Mybatis4/src/com/river/test/TestA.java
@@ -0,0 +1,49 @@
+package com.river.test;
+
+import com.river.entity.Student;
+import com.river.mapper.ClazzMapper;
+import com.river.mapper.StudentMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
+// 多表查询的第一种方法
+
+public class TestA {
+    public static void main(String[] args) throws IOException {
+        // 解析xml文件
+        InputStream inputStream = Resources.getResourceAsStream("mybatis.xml");
+
+        // 获得sqlSessionFactory工厂
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 获得session对象
+        SqlSession sqlSession = factory.openSession(true);
+
+        // 执行方法
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+        ClazzMapper clazzMapper = sqlSession.getMapper(ClazzMapper.class);
+
+        // 查询所有学生所在的班级信息
+        // a查询所有学生信息
+        List<Student> students = studentMapper.selectAll();
+
+        // b查询所有学生班级号的班级信息
+        for (Student student : students) {
+            System.out.println(student.getSname() + " ==> "
+                    + clazzMapper.selectOne(student.getClazzno()).getCname());
+        }
+
+
+        // 关闭sqlSession
+        sqlSession.close();
+        inputStream.close();
+
+    }
+}
