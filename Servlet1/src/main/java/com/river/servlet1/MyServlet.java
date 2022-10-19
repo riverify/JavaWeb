87284a6e172bd58dc457ade3f2a7ae7e229b644a
Riverify <CryingRiver@foxmail.com>
2022-10-19 10:10:57 +0800

    全局参数

--- Servlet1/src/main/java/com/river/servlet1/MyServlet.java
+++ Servlet1/src/main/java/com/river/servlet1/MyServlet.java
@@ -8,9 +8,28 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MyServlet extends HttpServlet {
+    String enc;
+    String enc2;
+
+    @Override
+    public void init() throws ServletException {
+        // 读取属性文件
+
+        // 读取初始化参数
+        enc = this.getInitParameter("enc");
+//        enc = this.getServletConfig().getInitParameter("enc");
+        // 读取全局初始化参数
+        enc2 = this.getServletContext().getInitParameter("enc-utf8");
+
+        // 输出
+        System.out.println("初始化参数：" + enc);
+        System.out.println("全局初始化参数：" + enc2);
+
+    }
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html;charset=utf-8"); // 防止中文乱码
+        resp.setContentType("text/html;charset=" + enc2); // 防止中文乱码
 
         PrintWriter out = resp.getWriter();
 
